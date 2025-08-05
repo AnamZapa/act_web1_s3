2f24dfd90c06f8a5a10f0b97a6289cd97535ecef
AnamZapa <azapatama1234567@cesde.net>
2025-08-05 13:30:35 -0500

    Actualización ejercicios

--- ejercicios-for/ejercicio_for_03.js
+++ ejercicios-for/ejercicio_for_03.js
@@ -1,3 +1,15 @@
 // Ejercicio For 3: Análisis de Array de Números
 // Dado un array de 20 números aleatorios, usa un ciclo for para encontrar: 
 // el número mayor, el menor, la suma total, el promedio, y cuántos números son pares e impares.
+
+let numeros = [];
+for (let i = 0; i < 20; i++){
+    numeros.push(Math.floor(Math.random())*100)
+}
+
+let mayor = 0
+let menor = 0
+let total = 0
+let promedio = 0
+let pares = 0
+let impares = 0
